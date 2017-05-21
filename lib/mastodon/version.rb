dbb8c07e05c13721cc21f5d3bdaad79450e9921a
Clworld <clworld@ggtea.org>
2017-05-21 21:43:04 +0900

    bug fix

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -20,24 +20,24 @@ module Mastodon
       2
     end
 	
-	class << self
-	  alias_method :original_minor, :minor
-	  alias_method :original_patch, :patch
-	end
-	@@minor = nil
-	def minor
-	  @@minor = !ENV['VERSION_MINOR'].nil? ? ENV['VERSION_MINOR'] : '' if @@minor.nil?
-	  !@@minor.nil? ? @@minor : original_minor
-	end
-	@@patch = nil
-	def patch
-	  @@patch = !ENV['VERSION_PATCH'].nil? ? ENV['VERSION_PATCH'] : '' if @@patch.nil?
-	  !@@patch.nil? ? @@patch : original_patch
-	end
+    class << self
+      alias_method :original_minor, :minor
+      alias_method :original_patch, :patch
+    end
+    @@minor = nil
+    def minor
+      @@minor = !ENV['VERSION_MINOR'].nil? ? ENV['VERSION_MINOR'] : nil if @@minor.nil?
+      !@@minor.nil? ? @@minor : original_minor
+    end
+    @@patch = nil
+    def patch
+      @@patch = !ENV['VERSION_PATCH'].nil? ? ENV['VERSION_PATCH'] : nil if @@patch.nil?
+      !@@patch.nil? ? @@patch : original_patch
+    end
 
-	@@append_version = nil
+    @@append_version = nil
     def append_version
-	  @@append_version = ENV['APPEND_VERSION'].present? ? ENV['APPEND_VERSION'] : '' if @@append_version.nil?
+    @@append_version = ENV['APPEND_VERSION'].present? ? ENV['APPEND_VERSION'] : '' if @@append_version.nil?
       @@append_version
     end
 
@@ -46,7 +46,7 @@ module Mastodon
     end
 
     def to_s
-	  append = append_version.present? ? " #{append_version}" : ''
+    append = append_version.present? ? " #{append_version}" : ''
       to_a.join('.') + append
     end
   end
